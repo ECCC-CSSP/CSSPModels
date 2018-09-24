1a3ab1e7057a74a550715cdbd45ce92a4b87ba09
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-09-24 17:33:06 -0300

    Init

--- CSSPModels/src/TVFileLanguage.cs
+++ CSSPModels/src/TVFileLanguage.cs
@@ -21,12 +21,28 @@ namespace CSSPModels
         [CSSPDescriptionFR(DescriptionFR = @"Contient l'identifiant unique sur chaque ligne de la table TVFileLanguages")]
         public int TVFileLanguageID { get; set; }
         [CSSPExist(ExistTypeName = "TVFile", ExistPlurial = "s", ExistFieldID = "TVFileID")]
+        [CSSPDisplayEN(DisplayEN = "TV file ID")]
+        [CSSPDisplayFR(DisplayFR = "Filière de l'arbre visuel ID")]
+        [CSSPDescriptionEN(DescriptionEN = @"Link to the TVFiles table representing the tree view file")]
+        [CSSPDescriptionFR(DescriptionFR = @"Lien à la table TVFiles représentant la filière de l'arbre visuel")]
         public int TVFileID { get; set; }
         [CSSPEnumType]
+        [CSSPDisplayEN(DisplayEN = "Language")]
+        [CSSPDisplayFR(DisplayFR = "Langage")]
+        [CSSPDescriptionEN(DescriptionEN = @"Language of item")]
+        [CSSPDescriptionFR(DescriptionFR = @"Langage de l'item")]
         public LanguageEnum Language { get; set; }
         [CSSPAllowNull]
+        [CSSPDisplayEN(DisplayEN = "File description")]
+        [CSSPDisplayFR(DisplayFR = "Description du fichier")]
+        [CSSPDescriptionEN(DescriptionEN = @"File description")]
+        [CSSPDescriptionFR(DescriptionFR = @"Description du fichier")]
         public string FileDescription { get; set; }
         [CSSPEnumType]
+        [CSSPDisplayEN(DisplayEN = "Translation status")]
+        [CSSPDisplayFR(DisplayFR = "Le statut de la traduction")]
+        [CSSPDescriptionEN(DescriptionEN = @"Translation status of the file description")]
+        [CSSPDescriptionFR(DescriptionFR = @"Le statut de la traduction de la description du fichier")]
         public TranslationStatusEnum TranslationStatus { get; set; }
         #endregion Properties in DB
 
@@ -48,6 +64,10 @@ namespace CSSPModels
         public TVItemLanguage LastUpdateContactTVItemLanguage { get; set; }
         [CSSPEnumTypeText(EnumTypeName = "LanguageEnum", EnumType = "Language")]
         [CSSPAllowNull]
+        [CSSPDisplayEN(DisplayEN = "Language text")]
+        [CSSPDisplayFR(DisplayFR = "Texte du langage")]
+        [CSSPDescriptionEN(DescriptionEN = @"Language text [en, fr]")]
+        [CSSPDescriptionFR(DescriptionFR = @"Texte du langage [en, fr]")]
         public string LanguageText { get; set; }
         [CSSPEnumTypeText(EnumTypeName = "TranslationStatusEnum", EnumType = "TranslationStatus")]
         [CSSPAllowNull]
