f63ce2ee41f181f10bf7202851faa1e591dcaa79
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-09-25 17:16:28 -0300

    Init

--- CSSPModels/src/VPScenarioLanguage.cs
+++ CSSPModels/src/VPScenarioLanguage.cs
@@ -21,12 +21,28 @@ namespace CSSPModels
         [CSSPDescriptionFR(DescriptionFR = @"Contient l'identifiant unique sur chaque ligne de la table VPScenarioLanguages")]
         public int VPScenarioLanguageID { get; set; }
         [CSSPExist(ExistTypeName = "VPScenario", ExistPlurial = "s", ExistFieldID = "VPScenarioID")]
+        [CSSPDisplayEN(DisplayEN = "VP scenario ID")]
+        [CSSPDisplayFR(DisplayFR = "Scénario VP ID")]
+        [CSSPDescriptionEN(DescriptionEN = @"Link to the VPScenarios table representing the visual plumes scenario")]
+        [CSSPDescriptionFR(DescriptionFR = @"Lien à la table VPScenarios représentant le scénario visual plumes")]
         public int VPScenarioID { get; set; }
         [CSSPEnumType]
+        [CSSPDisplayEN(DisplayEN = "Language")]
+        [CSSPDisplayFR(DisplayFR = "Langage")]
+        [CSSPDescriptionEN(DescriptionEN = @"Language of item")]
+        [CSSPDescriptionFR(DescriptionFR = @"Langage de l'item")]
         public LanguageEnum Language { get; set; }
         [StringLength(100)]
+        [CSSPDisplayEN(DisplayEN = "VP scenario name")]
+        [CSSPDisplayFR(DisplayFR = "Nom du scénario VP")]
+        [CSSPDescriptionEN(DescriptionEN = @"Visual plumes scenario name")]
+        [CSSPDescriptionFR(DescriptionFR = @"Nom du scénario visual plumes")]
         public string VPScenarioName { get; set; }
         [CSSPEnumType]
+        [CSSPDisplayEN(DisplayEN = "Translation status")]
+        [CSSPDisplayFR(DisplayFR = "Le statut de la traduction")]
+        [CSSPDescriptionEN(DescriptionEN = @"Translation status of the visual plumes scenario name")]
+        [CSSPDescriptionFR(DescriptionFR = @"Le statut de la traduction du nom du scénario visual plumes")]
         public TranslationStatusEnum TranslationStatus { get; set; }
         #endregion Properties in DB
 
@@ -48,6 +64,10 @@ namespace CSSPModels
         public TVItemLanguage LastUpdateContactTVItemLanguage { get; set; }
         [CSSPEnumTypeText(EnumTypeName = "LanguageEnum", EnumType = "Language")]
         [CSSPAllowNull]
+        [CSSPDisplayEN(DisplayEN = "Language text")]
+        [CSSPDisplayFR(DisplayFR = "Texte du langage")]
+        [CSSPDescriptionEN(DescriptionEN = @"Language text [en, fr]")]
+        [CSSPDescriptionFR(DescriptionFR = @"Texte du langage [en, fr]")]
         public string LanguageText { get; set; }
         [CSSPEnumTypeText(EnumTypeName = "TranslationStatusEnum", EnumType = "TranslationStatus")]
         [CSSPAllowNull]
